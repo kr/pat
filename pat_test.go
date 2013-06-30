fb2e91e4c3ecfc60c39d864f8bfed1f4adba7dd1
Timothée Peignier <timothee.peignier@tryphon.org>
2013-06-30 13:58:45 -0700

    renaming files

--- pat_test.go
+++ pat_test.go
@@ -238,3 +238,17 @@ func TestTail(t *testing.T) {
 		t.Fatalf("want: %q, got %q", "", g)
 	}
 }
+
+func BenchmarkPatternMatching(b *testing.B) {
+	p := New()
+	p.Get("/hello/:name", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		r, err := http.NewRequest("GET", "/hello/blake", nil)
+		if err != nil {
+			panic(err)
+		}
+		b.StartTimer()
+		p.ServeHTTP(nil, r)
+	}
+}
